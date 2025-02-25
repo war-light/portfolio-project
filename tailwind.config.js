ff3c6958991bb2ec6d9cae046c0526aaffb0e367
Honor Shard <134319458+JDSpaceBoy@users.noreply.github.com>
2025-02-25 19:09:16 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+export default {
+  content: ["./src/**/*.tsx", "./src/**/*.css"],
+  plugins: [require("@tailwindcss/forms")],
+};
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+    },
+    resolve: {
+      alias: {
+        app: resolve(__dirname, "src", "app"),
+        components: resolve(__dirname, "src", "components"),
+        hooks: resolve(__dirname, "src", "hooks"),
+      },
+    },
+  };
+});
