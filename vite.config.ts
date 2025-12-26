3c58ebfa7a903ce7cbc2eda563ddd5f577be89be
war-light <jd.stormlight@gmail.com>
2025-12-26 16:40:38 -0400

    feat: Add GitHub Pages deployment workflow, scripts, and base path configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
+    base: "/portfolio-project/",
     plugins: [react(), tailwindcss()],
     server: {
       port: 3000,
